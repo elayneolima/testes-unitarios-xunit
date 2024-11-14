fa78b5c66691a76057db7332edb7b50b4cd0b04b
elayneolima <elayne04lima@gmail.com>
2024-11-14 14:24:50 -0300

    aicionando mais cenarios de testes ao projeto

--- CalculadoraTestes/CalculadoraTests.cs
+++ CalculadoraTestes/CalculadoraTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.Design;
 using Calculadora.Services;
 
 namespace CalculadoraTestes;
@@ -24,4 +25,61 @@ public class CalculadoraTests
         Assert.Equal(15, resultado);
 
     }
+
+    [Fact]
+    public void DeveSomar10Com10ERetornar20()
+    {
+        //Arrange
+        int num1 = 10;
+        int num2 = 10; 
+
+        //Act
+        int resultado = _calc.Somar(num1, num2);
+
+        //Assert
+        Assert.Equal(20, resultado);
+
+    }
+
+   /* [Fact]
+    public void DeveVerificarSe4EhParERetornarVerdaeiro()
+    {
+        //Arrange
+        int numero = 4;
+
+        //Act
+        bool resultado = _calc.NumPar(numero);
+        //Assert
+        Assert.True(resultado);
+
+    }
+
+   /* [Theory]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(10)]
+    
+    public void DeveVerificarSeOsNumerosSaoParesERetornarVerdadeiro(int numero)
+    {
+        //Arrange
+        
+        //Act
+        bool resultado = _calc.NumPar(numero);
+        //Assert
+        Assert.True(resultado);
+    } */ 
+    
+    [Theory]
+    [InlineData(new int[] {2, 4, 6, 8, 10})]
+    public void DeveVerificarSeOsNumerosSaoParesERetornarVerdadeiro(int[] numeros)
+    {
+        //Arrange
+        
+        //Act // //Assert
+        Assert.All(numeros, num => Assert.True(_calc.NumPar(num)));
+    }
+    
+
 }
